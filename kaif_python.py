4a31f9464a3af2074340ad7c3cba16e83122462e
kaif367 <96043855+kaif367@users.noreply.github.com>
2025-07-02 10:45:51 +0530

    Update kaif_python.py

--- kaif_python.py
+++ kaif_python.py
@@ -1,11 +1,22 @@
 import websocket
 
 def on_message(ws, message):
-    print("Message received:", message)
+    print("Received:", message)
+
+def on_error(ws, error):
+    print("Error:", error)
+
+def on_close(ws):
+    print("### closed ###")
 
 def on_open(ws):
-    print("Connected to Quotex WebSocket")
+    print("Connected to server")
 
 url = "wss://ws2.qxbroker.com/socket.io/?EIO=3&transport=websocket"
-ws = websocket.WebSocketApp(url, on_message=on_message)
+ws = websocket.WebSocketApp(url,
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close,
+                            on_open=on_open)
+
 ws.run_forever()
