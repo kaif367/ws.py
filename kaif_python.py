03300f7af4e15de71a97ce3d4cf90767401b3c6c
kaif367 <96043855+kaif367@users.noreply.github.com>
2025-07-02 10:51:22 +0530

    Update kaif_python.py

--- kaif_python.py
+++ kaif_python.py
@@ -7,16 +7,19 @@ def on_error(ws, error):
     print("Error:", error)
 
 def on_close(ws):
-    print("### closed ###")
+    print("### Connection closed ###")
 
 def on_open(ws):
-    print("Connected to server")
+    print("Connected to Quotex WebSocket")
 
-url = "wss://ws2.qxbroker.com/socket.io/?EIO=3&transport=websocket"
-ws = websocket.WebSocketApp(url,
-                            on_message=on_message,
-                            on_error=on_error,
-                            on_close=on_close,
-                            on_open=on_open)
+def main():
+    url = "wss://ws2.qxbroker.com/socket.io/?EIO=3&transport=websocket"
+    ws = websocket.WebSocketApp(url,
+                                 on_message=on_message,
+                                 on_error=on_error,
+                                 on_close=on_close,
+                                 on_open=on_open)
+    ws.run_forever()
 
-ws.run_forever()
+if __name__ == "__main__":
+    main()
